870537c96f7426823a78068ddf908ebf6b2d2459
David Legrand <david.legrand@acseo-conseil.fr>
2024-11-12 00:20:17 +0100

    CTO update

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('src/img')
   eleventyConfig.addPassthroughCopy('src/robots.txt')
+  eleventyConfig.addPassthroughCopy('src/.htaccess')
   eleventyConfig.addCollection("sections", function (collectionApi) {
     return collectionApi.getAll()
       .filter((a) => a.data.tags.includes('sections'))
